5c4979965dc3fe7d385405c9f29a1e2a71e8e6eb
Ruchi Gupta <70604743+ruchi-gupta94@users.noreply.github.com>
2020-12-22 21:03:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,92 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10;
+var box11, box12, box13, box14, box15, box16, box17, box18, box19, box20;
+var ground;
+var ball, rope;
+
+function setup() {
+  createCanvas(3000, 800);
+  engine = Engine.create();
+  world = engine.world;
+
+  //creating ground
+  ground = new Ground(800, 600, 1600, 20);
+
+  //creating boxes
+  box1 = new Box(900, 100, 70, 70);
+  box2 = new Box(900, 100, 70, 70);
+  box3 = new Box(900, 100, 70, 70);
+  box4 = new Box(900, 100, 70, 70);
+  box5 = new Box(900, 100, 70, 70);
+  box6 = new Box(900, 100, 70, 70);
+  box7 = new Box(800, 100, 70, 70);
+  box8 = new Box(800, 100, 70, 70);
+  box9 = new Box(800, 100, 70, 70);
+  box10 = new Box(800, 100, 70, 70);
+  box11 = new Box(800, 100, 70, 70);
+  box12 = new Box(800, 100, 70, 70);
+  box13 = new Box(700, 100, 70, 70);
+  box14 = new Box(700, 100, 70, 70);
+  box15 = new Box(700, 100, 70, 70);
+  box16 = new Box(700, 100, 70, 70);
+  box17 = new Box(700, 100, 70, 70);
+  box18 = new Box(700, 100, 70, 70);
+  box19 = new Box(700, 100, 70, 70);
+  box20 = new Box(700, 100, 70, 70);
+
+  //creating ball
+  ball = new Ball(200, 200, 80, 80);
+
+  //creating rope and attaching the ball
+  rope = new Rope(ball.body, { x: 500, y: 50 });
+
+}
+
+function draw() {
+  background(180);
+  Engine.update(engine);
+
+  //displaying the ground
+  ground.display();
+
+  //displaying the boxes
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display()
+  box5.display()
+  box6.display()
+  box7.display()
+  box8.display()
+  box9.display()
+  box10.display()
+  box11.display()
+  box12.display()
+  box13.display()
+  box14.display()
+  box15.display()
+  box16.display()
+  box17.display()
+  box18.display()
+  box19.display()
+  box20.display()
+  
+  //displaying the ball
+  ball.display();
+
+  //displaying the rope
+  rope.display();
+
+}
+
+
+function mouseDragged() {
+  Matter.Body.setPosition(ball.body, { x: mouseX, y: mouseY });
+}
+
+
